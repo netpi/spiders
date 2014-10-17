e7e7baec5c20f52b8d042b84c6d2449b48490d6c
netpi <netpi@foxmail.com>
2014-10-18 01:46:01 +0800

    fix

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ wechattools.weather = require('./lib/weather');
 wechattools.history = require('./lib/history');
 wechattools.translate = require('./lib/translate');
 wechattools.face = require('./lib/face');
+
 module.exports = wechattools;
